1efe0a217cbb0ac44f4dd4ba652589459ae276b5
sarthak patel <patelsarthak421@gmail.com>
2022-05-26 14:16:31 +0530

    added search bar

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 //jshint esversion:6
 
-
+//requiring
 const express = require("express");
 const bodyparser = require("body-parser");
 const mongoose = require("mongoose");
 
 
 const app = express();
-var filterbySubject="";
+
+//initialising variables
+var filterbySubject = "";
+var commentid="";
 
 app.set('view engine', 'ejs');
 app.use(express.static("public"));
@@ -19,6 +22,8 @@ app.use(bodyparser.urlencoded({
 mongoose.connect("mongodb://localhost:27017/userDB", {
   useNewUrlParser: true
 });
+
+//storing user data (email and passwords)
 const userSchema = {
   email: String,
   password: String
@@ -26,14 +31,16 @@ const userSchema = {
 const User = new mongoose.model("User", userSchema)
 
 
+//storing notes information
 const noteSchema = {
-  yourname:String,
-  proffname:String,
-  subject:String,
-  date:String,
-  chapname:String,
-  linktitle:String,
-  link:String
+  yourname: String,
+  proffname: String,
+  subject: String,
+  date: String,
+  chapname: String,
+  linktitle: String,
+  link: String,
+  comment:[String]
 };
 
 const Note = new mongoose.model("Note", noteSchema)
@@ -63,24 +70,25 @@ app.get("/connect", function(req, res) {
   res.sendFile(__dirname + "/connect.html");
 });
 
-app.post("/information",function(req,res){
+app.post("/information", function(req, res) {
 
-  const yourname=req.body.your_name
-  const proffname=req.body.professors_name
-  const subject=req.body.subjects
-  const date=req.body.date
-  const chapname=req.body.chapter_name
-  const linktitle=req.body.link_title
-  const link=req.body.notes_link
+  const yourname = req.body.your_name
+  const proffname = req.body.professors_name
+  const subject = req.body.subjects
+  const date = req.body.date
+  const chapname = req.body.chapter_name
+  const linktitle = req.body.link_title
+  const link = req.body.notes_link
 
-  const note=new Note({
-    yourname:yourname,
-    proffname:proffname,
-    subject:subject,
-    date:date,
-    chapname:chapname,
-    linktitle:linktitle,
-    link:link
+
+  const note = new Note({
+    yourname: yourname,
+    proffname: proffname,
+    subject: subject,
+    date: date,
+    chapname: chapname,
+    linktitle: linktitle,
+    link: link
   })
 
   note.save();
@@ -90,30 +98,54 @@ app.post("/information",function(req,res){
 
 app.get("/list", function(req, res) {
 
-  Note.find({subject:filterbySubject},function(err,foundItems){
-    if(foundItems.length===0) console.log("no resources found");
-
-    else{
-      res.render("list",{subject:filterbySubject,newListItems:foundItems});
-    }
+  Note.find({
+    subject: filterbySubject
+  }, function(err, foundItems) {
+    res.render("list", {
+      subject: filterbySubject,
+      newListItems: foundItems
+    });
 
   })
 
 
 });
 
+app.get("/comment", function(req, res) {
+  Note.find({
+    _id: commentid
+  }, function(err, foundItems) {
 
-app.post("/list",function(req,res){
-  filterbySubject=req.body.button
+    res.render("comment", {
+      listTitle: commentid,
+      newListItems: foundItems,
+    });
 
+  })
+
+});
+
+
+app.post("/list", function(req, res) {
+  console.log(req.body.button)
+  filterbySubject = req.body.button
   res.redirect("/list")
+});
+
+
+//comment post request
+app.post("/comment", function(req, res) {
+  commentid=req.body.button
+
+  res.redirect("/comment")
+
 });
 
 
 app.post("/", function(req, res) {
 
-  const username=req.body.username
-  const password=req.body.password
+  const username = req.body.username
+  const password = req.body.password
   User.findOne({
     email: username
   }, function(err, foundUser) {
